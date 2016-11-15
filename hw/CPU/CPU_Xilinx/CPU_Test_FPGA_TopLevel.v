fda086502ac69f5d2b49f50697ec98725c5a1a1f
Jonathan Brand <jbrand989@gmail.com>
2016-11-15 14:18:41 -0600

    Xilinx updates

--- hw/CPU/CPU_Xilinx/CPU_Test_FPGA_TopLevel.v
+++ hw/CPU/CPU_Xilinx/CPU_Test_FPGA_TopLevel.v
@@ -21,6 +21,14 @@
 module CPU_Test_FPGA_TopLevel(
 	input clk, 
 	input rst,
+	output GPIO_LED_0, 
+	output GPIO_LED_1, 
+	output GPIO_LED_2, 
+	output GPIO_LED_3, 
+	output GPIO_LED_4, 
+	output GPIO_LED_5, 
+	output GPIO_LED_6, 
+	output GPIO_LED_7, 
 	output txd
 );
 	
@@ -29,7 +37,7 @@ module CPU_Test_FPGA_TopLevel(
 	wire [31:0] data_mem_write_data;
 	wire data_mem_wr;
 	wire data_mem_en;
-	wire [31:0] PC_curr;
+	wire [31:0] instr_addr;
 	wire HALTED; 
    
 	// Inputs to CPU
@@ -46,6 +54,16 @@ module CPU_Test_FPGA_TopLevel(
 	assign real_data_mem_wr = (data_mem_addr != 32'h10000000 && data_mem_wr) ? 1'b1 : 1'b0;
 	assign real_data_read =   (data_mem_addr == 32'h10000004) ?  {31'd0, tbr} : data_mem_data;
 	
+	// LED debug 
+	assign GPIO_LED_0 = 0;//(instruction == 32'h00000010);
+	assign GPIO_LED_1 = 0;//(instruction == 32'h200b0064);
+	assign GPIO_LED_2 = 0;//(instruction == 32'h200c0064);
+	assign GPIO_LED_3 = 0;//(instruction == 32'h200d00c8);
+	assign GPIO_LED_4 = 0;//(instruction == 32'h43870000);
+	assign GPIO_LED_5 = 0;//(instruction == 32'h47bb0000);
+	assign GPIO_LED_6 = 0;//(instruction == 32'h3360fff8);
+	assign GPIO_LED_7 = HALTED;//(instruction == 32'h00000000);
+	
 	proc PROC (
 		.clk(clk),
 		.rst(rst),
@@ -53,7 +71,7 @@ module CPU_Test_FPGA_TopLevel(
 		.data_mem_write_data(data_mem_write_data),
 		.data_mem_wr(data_mem_wr),
 		.data_mem_en(data_mem_en),
-		.PC_curr(PC_curr),
+		.instr_addr(instr_addr),
 		.data_mem_data(real_data_read),
 		.instruction(instruction),
 		.HALTED(HALTED),
@@ -68,7 +86,7 @@ module CPU_Test_FPGA_TopLevel(
 	IMEM i_mem (
 		.clka(clk),
         .rsta(rst),
-		.addra(PC_curr),
+		.addra(instr_addr),
 		.douta(instruction)
 	);
 	
@@ -80,9 +98,29 @@ module CPU_Test_FPGA_TopLevel(
 		.dina(data_mem_write_data),
 		.douta(data_mem_data)
 	);
-
-
-
+/*
+		memory2c #(0) d_mem(
+		.data_out(data_mem_data),
+		.data_in(data_mem_write_data),
+		.addr(data_mem_addr),
+		.enable(1'b1),
+		.wr(real_data_mem_wr),
+		.createdump(1'b0),
+		.clk(clk),
+		.rst(rst)
+	);
+	
+	memory2c #(1) i_mem(
+		.data_out(instruction),
+		.data_in(32'h0),
+		.addr(instr_addr),
+		.enable(1'b1),
+		.wr(1'b0),
+		.createdump(1'b0),
+		.clk(clk),
+		.rst(rst)
+	);
+*/
 	assign trmt = (data_mem_addr == 32'h10000000 && data_mem_wr) ? 1'b1 : 1'b0;
 	
 	spart_tx SPART(
@@ -92,7 +130,7 @@ module CPU_Test_FPGA_TopLevel(
 		.tx_data(data_mem_write_data[7:0]),
 		.load_baud(1'b1),
 		.baud_val(16'h0a2c),
-        //.baud_val(16'h0010),
+      //.baud_val(16'h0010),
 		.TBR(tbr), 
 		.TX(txd)
 	);
